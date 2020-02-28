a6ab0f815a23f4059c5256718c87ec963e5895be
Daryna Bondarenko <d.bondarenko@aimprosoft.com>
2020-02-28 19:05:11 +0200

    ini commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const mapDispatchToProps = { addNewTask };
 
 function App({ addNewTask }) {
   return (
+
     <div className="container">
       <h1>Home</h1>
         <Board addNewTask={addNewTask} />
